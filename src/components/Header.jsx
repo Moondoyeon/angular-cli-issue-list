793586ebfe798ea458bfe7da371c9658398b625b
Moondoyeon <mmmdo21@gmail.com>
2022-11-08 16:03:10 +0900

    Feat: 헤더 컴포넌트

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,57 +1,45 @@
 /* eslint-disable react/prop-types */
-import styled from 'styled-components';
-import { IoChevronBackOutline } from 'react-icons/io5';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { flexBox } from '../styles/mixin';
 
-function Header({ isDetail }) {
+const CustomHeader = styled.header`
+  ${flexBox()};
+  height: 80px;
+  width: 100vw;
+  border-bottom: 1px solid;
+  border-color: ${({ theme }) => theme.lightGray};
+  background-color: ${({ theme }) => theme.gray};
+  position: sticky;
+  top: 0;
+  z-index: 1;
+`;
+const Wrapper = styled.div`
+  cursor: pointer;
+  ${flexBox()};
+  font-size: 20px;
+  color: ${({ theme }) => theme.blue};
+  .repo {
+    font-weight: 700;
+  }
+  span {
+    margin: 0 5px;
+    color: black;
+  }
+`;
+function Header() {
   const navigate = useNavigate();
-  const goPreviousPage = () => {
-    navigate(-1);
+  const gotoHome = () => {
+    navigate('/');
   };
   return (
     <CustomHeader>
-      {isDetail ? (
-        <BtnWrapper>
-          <IoChevronBackOutline className="button" onClick={goPreviousPage} />
-        </BtnWrapper>
-      ) : (
-        <div className="side" />
-      )}
-      <div className="center">
-        <span>Angular</span> / <span className="repo">Angular-cli</span>
-      </div>
-      <div className="side" />
+      <Wrapper onClick={gotoHome}>
+        <div>angular</div>
+        <span>/</span>
+        <div className="repo">angular-cli</div>
+      </Wrapper>
     </CustomHeader>
   );
 }
 export default Header;
-const CustomHeader = styled.header`
-  padding-top: 20px;
-  padding-bottom: 20px;
-  display: flex;
-  align-items: center;
-  background-color: ${({ theme }) => theme.mainColor};
-
-  .side {
-    width: 15%;
-  }
-  .center {
-    width: 70%;
-    text-align: center;
-    span {
-      color: ${({ theme }) => theme.pointColor};
-      font-size: 18px;
-    }
-    .repo {
-      font-weight: 700;
-    }
-  }
-`;
-const BtnWrapper = styled.div`
-  width: 15%;
-  padding-left: 10px;
-  .button {
-    font-size: 30px;
-    cursor: pointer;
-  }
-`;
